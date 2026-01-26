b59bdc8e6d2c22f742de6c0e7e53024e2c69c1ba
VicenteAsmussen <vicente.asmussen2201@alumnos.ubiobio.cl>
2026-01-26 13:52:58 -0300

    Se creo la validacion de token y admin
    se creo el middleware que valida que el usuario tenga un token valido y otra validacion de que el usuario sea administrador, para futuras funciones exclusivas de este

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+
+export const verificarToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ mensaje: "Acceso denegado. No hay token." });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decodificado = jwt.verify(token, process.env.JWT_SECRET);
+    
+    req.usuario = decodificado; 
+    
+    next();
+  } catch (error) {
+    return res.status(403).json({ mensaje: "Token inválido o expirado." });
+  }
+};
+
+export const esAdministrador = (req, res, next) => {
+  if (req.usuario.rol !== "administrador") {
+    return res.status(403).json({ mensaje: "Acceso denegado. Se requiere rol de administrador." });
+  }
+  next();
+};
